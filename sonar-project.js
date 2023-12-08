cb2a7a7e2f8fb21e0a46a9396f810f5e36dcc35f
Madhusudhan <121033427+Madhusudhan1990@users.noreply.github.com>
2023-12-08 18:22:43 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://43.204.110.128:9000/',
+  serverUrl: 'http://http://52.66.44.139:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_a29489bac26997115705a265de45af9fcd166ce3',
+	    'sonar.login': 'squ_f468c13fa8e239465bf1daf17dda6bcacf3295a0',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'password',
             'sonar.projectVersion':'1.0',
